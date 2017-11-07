98fec972795d9eedaeff1037308276337c900513
Victor <vpangng@gmail.com>
2017-11-06 22:30:30 -0800

    3 basic projects

---
+++ weather/app.js
@@ -0,0 +1,28 @@
+function weather() {
+
+    var location = document.getElementById("location");
+    var apiKey = '49f019d243c3eec6c0f4343d44ccd276'; 
+    var url = 'https://api.forecast.io/forecast/';
+
+    navigator.geolocation.getCurrentPosition(success, error);
+
+    function success(position) {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+
+      location.innerHTML = 'Latitude is ' + latitude + '° Longitude is ' + longitude + '°';
+
+       $.getJSON(url + apiKey + "/" + latitude + "," + longitude + "?callback=?", function(data) {
+        $('#temp').html(data.currently.temperature + '° F');
+        $('#minutely').html(data.minutely.summary);
+      });
+    }
+
+    function error() {
+      location.innerHTML = "Unable to retrieve your location";
+    }
+
+    location.innerHTML = "Locating...";
+  }
+
+  weather();
